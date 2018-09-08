d2c20caae518b9f8c63c28fda14db6b4b4641e36
Diego Curbelo <diego@curbelo.com>
2018-09-07 21:10:33 -0300

    Add Elixir 1.6 support

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TheMovieDB.MixProject do
     [
       app: :themoviedb,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
